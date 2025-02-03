cfeaacbaed273a386e60391fe1bfe34d37eb43b5
tigger <tigger04@gmail.com>
2025-02-03 16:43:49 +0000

    tantrum:acp

--- sanitize-writing-regex.pl
+++ sanitize-writing-regex.pl
@@ -1,23 +1,22 @@
-s/\bUa?f.sac\b/Uafásach/g
-s/\bUA?F.SACH\b/UAFÁSACH/g
-s/\bsean\b/SEÁN/gi
-s/\bceili\b/céilí/gi
-s/\bsusan\b/SUSAN/gi
-s/\bbecky\b/BECKY/gi
-s/\brhys\b/RHYS/gi
-s/\bricky\b/RICKY/gi
-s/\banna\b/ANNA/gi
-s/\bGardai\b/Gardaí/g
-s/\bGARDAI\b/GARDAí/g
-s/\btom\b/TOM/gi
-s/\btommy\b/TOMMY/gi
-s/\blulu\b/LULU/gi
-s/\blaura\b/LAURA/gi
-s/\balan\b/ALAN/gi
-s/\bbrian\b/BRIAN/gi
-s/\balice\b/ALICE/gi
-s/\bjessica\b/JESSICA/gi
-s/\bjess\b/JESS/gi
-s/\banonymous person\b/ANONYMOUS PERSON/gi
-s/\bnurse 2\b/NURSE 2/gi
-
+Ua?f.sac=Uafásach
+UA?F.SACH=UAFÁSACH
+sean=SEÁN
+ceili=céilí
+susan=SUSAN
+becky=BECKY
+rhys=RHYS
+ricky=RICKY
+anna=ANNA
+Gardai=Gardaí
+GARDAI=GARDAí
+tom=TOM
+tommy=TOMMY
+lulu=LULU
+laura=LAURA
+alan=ALAN
+brian=BRIAN
+alice=ALICE
+jessica=JESSICA
+jess=JESS
+anonymous person=ANONYMOUS PERSON
+nurse 2=NURSE 2
